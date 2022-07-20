dbe5f63cd0ace5cadca479b6b971be441b7cc74d
pinastirobiaulia <101853322+pinastirobiaulia@users.noreply.github.com>
2022-07-21 06:59:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php 
+include 'koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="all.min.css">
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> -->
+
+
+    <title>Halaman Login</title>
+  </head>
+  <body>
+  <!-- Form Login -->
+    <div class="container">
+      <h4 class="text-center">FORM LOGIN</h4>
+      <hr>
+      <form method="POST" action="">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Username</label>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fas fa-user"></i></div>
+              </div>
+              <input type="text" class="form-control" placeholder="Masukkan Username" name="username">
+            </div>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fas fa-unlock-alt"></i></div>
+              </div>
+              <input type="password" class="form-control" placeholder="Masukkan Password" name="password">
+          </div>
+        </div>
+        <div class="mb-3" >
+          <small><a href="register.php" class="text-dark">Belum Punya Akun ? Buat Akun Anda !</a></small>
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary">LOGIN</button>
+        <button type="reset" name="reset" class="btn btn-danger">RESET</button>
+      </form>
+  <!-- Akhir Form Login -->
+
+  <!-- Eksekusi Form Login -->
+      <?php 
+        if(isset($_POST['submit'])) {
+          $user = $_POST['username'];
+          $password = $_POST['password'];
+
+          // Query untuk memilih tabel
+          $cek_data = mysqli_query($koneksi, "SELECT * FROM user WHERE username = '$user' AND password = '$password'");
+          $hasil = mysqli_fetch_array($cek_data);
+          $status = $hasil['status'];
+          $login_user = $hasil['username'];
+          $row = mysqli_num_rows($cek_data);
+
+          // Pengecekan Kondisi Login Berhasil/Tidak
+            if ($row > 0) {
+                session_start();   
+                $_SESSION['login_user'] = $login_user;
+
+                if ($status == 'admin') {
+                  header('location: utamaa.php');
+                }elseif ($status == 'user') {
+                  header('location: utamaa.php'); 
+                }
+            }else{
+                header("location: index.php");
+            }
+        }
+       ?>
+    </div>
+  <!-- Akhir Eksekusi Form Login -->
+
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="all.min.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+
+    <title>Halaman Registrasi</title>
+  </head>
+  <body>
+ 
+ <!-- Form Registrasi -->
+  <div class="container">
+    <h3 class="text-center mt-3 mb-5">HALAMAN REGISTRASI</h3>
+    <div class="card p-5 mb-5">
+    <form method="POST" action="simpan_register.php">
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="user">Username</label>
+          <input type="text" class="form-control" id="user" name="username" placeholder="Masukan Username">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="pass">Password</label>
+          <input type="password" class="form-control" id="pass" name="password" placeholder="Masukan Password">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="nama">Nama Lengkap</label>
+        <input type="text" class="form-control" id="nama" name="nama_lengkap" placeholder="Masukan Nama Lengkap">
+      </div>
+      <div class="form-group">
+        <label for="jk">Jenis Kelamin</label><br>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" name="jenis_kelamin" id="jk" value="Laki-Laki">
+          <label class="form-check-label" for="jk">Laki-Laki</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" name="jenis_kelamin" id="jk" value="Perempuan">
+          <label class="form-check-label" for="jk">Perempuan</label>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="tgl">Tanggal Lahir</label>
+        <input type="date" class="form-control" id="tgl" name="tanggal_lahir">
+      </div>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="rumah">Alamat</label>
+          <input type="text" class="form-control" id="rumah" name="alamat" placeholder="Masukan Alamat">
+        </div>
+        <div class="form-group col-md-2">
+          <label for="telp">No. Telephone</label>
+          <input type="text" class="form-control" id="telp" name="hp" placeholder="No. Telephone">
+        </div>
+      <div class="form-group col-md-4">
+          <label for="sts">Status Registrasi</label>
+          <select id="sts" class="form-control" name="status">
+            <option selected>Pilih...</option>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </select>
+        </div>
+      </div>     
+      <button type="register" class="btn btn-primary">Register</button>
+      <button type="reset" class="btn btn-danger">Reset</button>
+    </form>
+  </div>
+  </div>
+  <!-- Akhir Form Registrasi -->
+
+  </body>
+</html>
